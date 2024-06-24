35a4235b1768537c0163fb93108c09cf06ffecc6
FireRainV <youaredied1234@gmail.com>
2024-06-24 21:45:38 +0300

    +1 damage to iceshock

--- libraries/magical-glass/scripts/data/spells/ice_shock.lua
+++ libraries/magical-glass/scripts/data/spells/ice_shock.lua
@@ -8,7 +8,7 @@ end
 function spell:getDamage(user, target)
     if Game:isLight() then
         local min_magic = Utils.clamp(user.chara:getStat("magic") - 3, 1, 999)
-        return math.ceil((min_magic * 9) + 35 + Utils.random(5))
+        return math.ceil((min_magic * 9) + 36 + Utils.random(5))
     else
         return super.getDamage(self, user, target)
     end
