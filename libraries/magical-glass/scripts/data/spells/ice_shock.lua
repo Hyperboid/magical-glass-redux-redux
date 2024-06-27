2acbcf5cf3369864bce6d3a08926237865898487
FireRainV <youaredied1234@gmail.com>
2024-06-27 08:54:56 +0300

    Revert "increased min_magic"
    This reverts commit ea3dd0e72719f01c20df2ea61ea318379be1ec6b.

--- libraries/magical-glass/scripts/data/spells/ice_shock.lua
+++ libraries/magical-glass/scripts/data/spells/ice_shock.lua
@@ -7,8 +7,8 @@ end
 
 function spell:getDamage(user, target)
     if Game:isLight() then
-        local min_magic = Utils.clamp(user.chara:getStat("magic") - 2, 1, 999)
-        return math.ceil((min_magic * 9) + 27 + Utils.random(5))
+        local min_magic = Utils.clamp(user.chara:getStat("magic") - 3, 1, 999)
+        return math.ceil((min_magic * 9) + 36 + Utils.random(5))
     else
         return super.getDamage(self, user, target)
     end
