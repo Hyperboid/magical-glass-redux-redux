bd5a0205c3c18ba96fe63acc5dc127571d7e9f87
FireRainV <youaredied1234@gmail.com>
2024-07-03 18:49:41 +0300

    Update lightactionbox.lua

--- libraries/magical-glass/scripts/lightbattle/ui/lightactionbox.lua
+++ libraries/magical-glass/scripts/lightbattle/ui/lightactionbox.lua
@@ -59,9 +59,7 @@ function LightActionBox:createButtons()
             button.actbox = self
             table.insert(self.buttons, button)
             self:addChild(button)
-        elseif type(btn) == "boolean" then
-            -- nothing, used to create an empty space
-        else
+        elseif not type(btn) == "boolean" then -- nothing, used to create an empty space
             btn:setPosition(math.floor(66 + ((i - 1) * 156)) + 0.5, 183)
             btn.battler = self.battler
             btn.actbox = self
