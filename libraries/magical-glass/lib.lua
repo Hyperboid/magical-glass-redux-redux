d95a988159ec526581136c17772c9336c86744d8
FireRainV <youaredied1234@gmail.com>
2024-07-03 15:09:22 +0300

    DARK ITEMs message

--- libraries/magical-glass/lib.lua
+++ libraries/magical-glass/lib.lua
@@ -882,6 +882,27 @@ function lib:init()
 
             return new_inventory
         end)
+        
+        Utils.hook(LightInventory, "tryGiveItem", function(orig, self, item, ignore_dark)
+            if Game.inventory:hasItem("light/ball_of_junk") then
+                orig(self, item, ignore_dark)
+            else
+                if type(item) == "string" then
+                    item = Registry.createItem(item)
+                end
+                if ignore_dark or item.light then
+                    return super.tryGiveItem(self, item, ignore_dark)
+                else
+                    local dark_inv = self:getDarkInventory()
+                    local result = dark_inv:addItem(item)
+                    if result then
+                        return true, "* ([color:yellow]"..item:getName().."[color:reset] was added to your [color:yellow]DARK ITEMs[color:reset].)"
+                    else
+                        return false, "* (You have too many [color:yellow]DARK ITEMs[color:reset] to take [color:yellow]"..item:getName().."[color:reset].)"
+                    end
+                end
+            end
+        end)
     end
     Utils.hook(LightEquipItem, "convertToDark", function(orig, self, inventory) return false end)
     
