44e06a893c362695050463978b10d1375d8564ab
FireRainV <youaredied1234@gmail.com>
2024-07-24 20:25:41 +0300

    more stuff

--- scripts/battle/lightencounters/dummy.lua
+++ scripts/battle/lightencounters/dummy.lua
@@ -13,54 +13,26 @@ function Dummy:init()
     self:addEnemy("dummy")
 
     self.bg_siners = {0, 15, 30, 45, 60, 75}
-
-    self.offset = 0
 end
 
 function Dummy:update()
     super.update(self)
 
-    if self:getFlag("deltarune") then
-        self.offset = self.offset + 1 * DTMULT
-
-        if self.offset > 100 then
-            self.offset = self.offset - 100
-        end
-    else
-        for i = 1, #self.bg_siners do
-            self.bg_siners[i] = self.bg_siners[i] + DTMULT
-        end
+    for i = 1, #self.bg_siners do
+        self.bg_siners[i] = self.bg_siners[i] + DTMULT
     end
 
 end
 
 function Dummy:drawBackground()
-    if self:getFlag("deltarune") then
-
-        love.graphics.setLineStyle("rough")
+    local offset = 0
+    for i = 1, 6 do
+        local sine = (math.sin(self.bg_siners[i] / 14) * 8) + 12
+        Draw.setColor(0, 107/255, 183/255)
         love.graphics.setLineWidth(1)
-
-        for i = 2, 16 do
-            Draw.setColor(0, 61 / 255, 17 / 255, 1 / 2)
-            love.graphics.line(0, -210 + (i * 50) + math.floor(self.offset / 2), 640, -210 + (i * 50) + math.floor(self.offset / 2))
-            love.graphics.line(-200 + (i * 50) + math.floor(self.offset / 2), 0, -200 + (i * 50) + math.floor(self.offset / 2), 480)
-        end
-
-        for i = 3, 16 do
-            Draw.setColor(0, 61 / 255, 17 / 255, 1)
-            love.graphics.line(0, -100 + (i * 50) - math.floor(self.offset), 640, -100 + (i * 50) - math.floor(self.offset))
-            love.graphics.line(-100 + (i * 50) - math.floor(self.offset), 0, -100 + (i * 50) - math.floor(self.offset), 480)
-        end
-    else
-        local offset = 0
-        for i = 1, 6 do
-            local sine = (math.sin(self.bg_siners[i] / 14) * 8) + 12
-            Draw.setColor(0, 107/255, 183/255)
-            love.graphics.setLineWidth(1)
-            love.graphics.rectangle("line", 18 + offset, sine, 101, 118)
-            love.graphics.rectangle("line", 18 + offset, sine + 118, 101, 118)
-            offset = offset + 101
-        end
+        love.graphics.rectangle("line", 18 + offset, sine, 101, 118)
+        love.graphics.rectangle("line", 18 + offset, sine + 118, 101, 118)
+        offset = offset + 101
     end
 end
 
