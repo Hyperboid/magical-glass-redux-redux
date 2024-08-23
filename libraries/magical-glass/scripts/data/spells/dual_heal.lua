83b7fbf73a47780929d3df709f1cdcd4bb1db447
FireRainV <youaredied1234@gmail.com>
2024-08-23 13:56:30 +0300

    shortened healing magic code

--- libraries/magical-glass/scripts/data/spells/dual_heal.lua
+++ libraries/magical-glass/scripts/data/spells/dual_heal.lua
@@ -1,15 +1,9 @@
 local spell, super = Class("dual_heal", true)
 
-function spell:onLightStart(user, target)
-    local amount = math.ceil(Game:isLight() and user.chara:getStat("magic") * 3 or user.chara:getStat("magic") * 5.5)
+function spell:onLightCast(user, target)
+    self.amount = math.ceil(Game:isLight() and user.chara:getStat("magic") * 3 or user.chara:getStat("magic") * 5.5)
     for _,battler in ipairs(Game.battle.party) do
-        battler:heal(amount, false, true)
-    end
-
-    local result = self:onLightCast(user, target)
-    Game.battle:battleText(self:getLightCastMessage(user, target))
-    if result or result == nil then
-        Game.battle:finishActionBy(user)
+        battler:heal(self.amount, false, true)
     end
 end
 
--- libraries/magical-glass/scripts/data/spells/ultimate_heal.lua
+++ libraries/magical-glass/scripts/data/spells/ultimate_heal.lua
@@ -1,14 +1,12 @@
 local spell, super = Class("ultimate_heal", true)
 
-function spell:onLightStart(user, target)
-    local amount = math.ceil(Game:isLight() and user.chara:getStat("magic") or user.chara:getStat("magic") + 1)
-    target:heal(amount, false, true)
+function spell:onLightCast(user, target)
+    self.amount = math.ceil(Game:isLight() and user.chara:getStat("magic") or user.chara:getStat("magic") + 1)
+    target:heal(self.amount, false, true)
+end
 
-    local result = self:onLightCast(user, target)
-    Game.battle:battleText(self:getLightCastMessage(user, target).."\n"..self:getHealMessage(user, target, amount))
-    if result or result == nil then
-        Game.battle:finishActionBy(user)
-    end
+function spell:getLightCastMessage(user, target)
+    return super.getLightCastMessage(self, user, target).."\n"..self:getHealMessage(user, target, self.amount)
 end
 
 return spell
