55305159e2a10e758d833e30262fdc3db4bc3f65
FireRainV <youaredied1234@gmail.com>
2024-10-04 23:22:42 +0300

    small fix

--- mod.lua
+++ mod.lua
@@ -2,11 +2,14 @@ function Mod:init()
     print("Loaded "..self.info.name.."!")
 end
 
-function Mod:load(new_file)
+function Mod:load(data, new_file)
     if new_file then
         Game.money = Kristal.getLibConfig("magical-glass", "debug") and 1000 or 0
         Game.lw_money = Kristal.getLibConfig("magical-glass", "debug") and 1000 or 0
         MagicalGlassLib:setLightBattleShakingText(true)
+        -- local party = Game:getPartyMember("noelle")
+        -- party:setLightEXP(69420)
+        -- party.lw_health = party.lw_stats.health
     end
     Game.world:registerCall("Dimensional Box A", "cell.box_a", false)
     Game.world:registerCall("Dimensional Box B", "cell.box_b", false)
