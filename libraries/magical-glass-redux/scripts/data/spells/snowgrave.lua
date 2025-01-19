9da8814b1ce024d2e0318b6bd5ca61a0b3d927a8
FireRainV <youaredied1234@gmail.com>
2025-01-19 16:37:07 +0200

    Revert "just the damage reduction"
    This reverts commit a143c8b40b2aeb41a16d668257f7c04bc3e9eaf6.

--- libraries/magical-glass-redux/scripts/data/spells/snowgrave.lua
+++ libraries/magical-glass-redux/scripts/data/spells/snowgrave.lua
@@ -6,12 +6,4 @@ function spell:init()
     self.check = "Deals the fatal damage to all of the enemies."
 end
 
-function spell:getDamage(user, target)
-    if Game:isLight() then
-        return math.ceil((user.chara:getStat("magic") * 35) + 560)
-    else
-        return super.getDamage(self, user, target)
-    end
-end
-
 return spell
